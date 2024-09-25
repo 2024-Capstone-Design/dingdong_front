8b338293922144089530df1e04cfe4e526369d5d
Chu Yulim <20102124@g.seoultech.ac.kr>
2024-09-26 03:03:08 +0900

    modify: change coding site url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'https://apis.ding-dong.xyz';
-export const CODING_SITE_URL = 'http://192.168.62.19:8601/';
+export const CODING_SITE_URL = 'http://3.39.80.113:8601/';
 export const FAST_API_BASE_URL = 'https://chatbot.ding-dong.xyz';
 // export const FAST_API_BASE_URL = 'http://127.0.0.1:8000';
